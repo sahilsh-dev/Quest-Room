bfecd191c57a62724a089ffb97a56d7d1f3d12e1
Sahil Sharma <vlsharma713@gmail.com>
2024-03-11 13:13:46 +0530

    Add home and sing up page

--- core/users/views.py
+++ core/users/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from .forms import RegisterForm
 
-# Create your views here.
+User = get_user_model()
+
 def sign_up(request):
+    form = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('rooms:home')
-    form = RegisterForm()
     return render(request, 'registration/sign-up.html', {'form': form})
