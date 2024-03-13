98d93d9d0e5183964bc5784e46718cf9ff87cbf8
Sahil Sharma <vlsharma713@gmail.com>
2024-03-13 18:07:38 +0530

    Create quest room model tests

--- core/rooms/tests.py
+++ core/rooms/tests.py
@@ -78,3 +78,61 @@ class CreateRoomViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(self.user.rooms.count(), 0)
     
+
+class QuestRoomModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('test', 'test')
+
+    def test_creator_is_head(self):
+        room = QuestRoom.objects.create(
+            name='Test Room', 
+            description='Test Description', 
+            expire_days=10, 
+            room_type=QuestRoom.RoomType.LEETCODE, 
+            daily_required_points=1,
+            head=self.user,
+            expires_at = timezone.now() + timezone.timedelta(days=10)
+        )
+        self.assertEqual(room.head, self.user)
+        
+    def test_room_expires_at(self):
+        room = QuestRoom.objects.create(
+            name='Test Room', 
+            description='Test Description', 
+            expire_days=10, 
+            room_type=QuestRoom.RoomType.LEETCODE, 
+            daily_required_points=1,
+            head=self.user,
+            expires_at = timezone.now() + timezone.timedelta(days=10)
+        )
+        self.assertLessEqual(room.expires_at, timezone.now() + timezone.timedelta(days=10))
+        
+    def test_room_members(self):
+        room = QuestRoom.objects.create(
+            name='Test Room', 
+            description='Test Description', 
+            expire_days=10, 
+            room_type=QuestRoom.RoomType.LEETCODE, 
+            daily_required_points=1,
+            head=self.user,
+            expires_at = timezone.now() + timezone.timedelta(days=10)
+        )
+        self.assertEqual(room.members.count(), 0)
+        room.members.add(self.user)
+        self.assertEqual(room.members.count(), 1)
+        self.assertEqual(room.members.first(), self.user)
+        room.members.remove(self.user)
+        self.assertEqual(room.members.count(), 0)
+        
+    def test_room_type(self):
+        room = QuestRoom.objects.create(
+            name='Test Room', 
+            description='Test Description', 
+            expire_days=10, 
+            room_type=QuestRoom.RoomType.LEETCODE, 
+            daily_required_points=1,
+            head=self.user,
+            expires_at = timezone.now() + timezone.timedelta(days=10)
+        )
+        self.assertEqual(room.room_type, QuestRoom.RoomType.LEETCODE)
+        
